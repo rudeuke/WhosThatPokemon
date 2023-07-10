3988388eb234220e2630aa7fe1c1fd97d6afeb35
BK <bartekqql@gmail.com>
2023-07-10 17:01:53 +0200

    Add Pokemon model

---
+++ Models/Pokemon.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhosThatPokemon.Models
+{
+    public class Pokemon
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string SilhouetteUrl { get; set; } = string.Empty;
+
+        public Pokemon(int id, string name, string imageUrl, string silhouetteUrl)
+        {
+            Id = id;
+            Name = name;
+            ImageUrl = imageUrl;
+            SilhouetteUrl = silhouetteUrl;
+        }
+    }
+}
