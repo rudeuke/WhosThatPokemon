410f51c3aae04dbfddffd8293f6baafbbd768f5b
BK <bartekqql@gmail.com>
2023-07-10 19:56:57 +0200

    Add basic controller for Pokemon model

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhosThatPokemon.Data;
+using WhosThatPokemon.Models;
+
+namespace WhosThatPokemon.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PokemonController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<Pokemon> GetPokemonById(int id)
+        {
+            var pokemon = _db.Pokemons.Find(id);
+            if (pokemon == null)
+            {
+                return NotFound();
+            }
+            return Ok(pokemon);
+        }
+    }
+}
