cf9e4618d235ba06bb27d270a326b1f31df7a717
BK <bartekqql@gmail.com>
2023-07-17 21:31:59 +0200

    Add JsonStringEnumConverter

--- Program.cs
+++ Program.cs
@@ -5,21 +5,30 @@ global using WhosThatPokemon.Dtos.Type;
 global using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using WhosThatPokemon.Data;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
-builder.Services.AddDbContext<ApplicationDbContext>(
-    options => options
-    .UseLazyLoadingProxies()
-    .UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    {
+        options
+        .UseLazyLoadingProxies()
+        .UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
 builder.Services.AddScoped<IPokemonService, PokemonService>();
 
 var app = builder.Build();
