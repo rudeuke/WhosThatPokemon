5d200e80f52f70cf795aa1b443279ac43cc70a73
BK <bartekqql@gmail.com>
2023-07-26 15:07:46 +0200

    Add validation rules for adding and updating pokemon

--- Validation/UpdatePokemonCommandValidator.cs
+++ Validation/UpdatePokemonCommandValidator.cs
@@ -3,17 +3,20 @@ using WhosThatPokemon.Commands;
 
 namespace WhosThatPokemon.Validation
 {
-    public class AddPokemonCommandValidator : AbstractValidator<AddPokemonCommand>
+    public class UpdatePokemonCommandValidator : AbstractValidator<UpdatePokemonCommand>
     {
-        public AddPokemonCommandValidator()
+        public UpdatePokemonCommandValidator()
         {
-            RuleFor(x => x.NewPokemonDto.OriginalId)
+            RuleFor(x => x.UpdatedPokemonDto.Id)
+                .GreaterThan(0)
+                .WithMessage("Id must be greater than 0");
+            RuleFor(x => x.UpdatedPokemonDto.OriginalId)
                 .InclusiveBetween(1, 1010)
                 .WithMessage("OriginalId must be between 1 and 1010");
-            RuleFor(x => x.NewPokemonDto.Name)
+            RuleFor(x => x.UpdatedPokemonDto.Name)
                 .NotEmpty()
                 .WithMessage("Name is required");
-            RuleFor(x => x.NewPokemonDto.PokemonTypes)
+            RuleFor(x => x.UpdatedPokemonDto.PokemonTypes)
                 .NotEmpty()
                 .WithMessage("Type is required")
                 .Must(pokemonTypes => pokemonTypes.Count >= 1 && pokemonTypes.Count <= 2)
