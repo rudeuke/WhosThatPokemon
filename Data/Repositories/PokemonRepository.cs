49545f80bd393bb097dde8b170b58bdb1b04e4de
BK <bartekqql@gmail.com>
2023-07-27 12:00:52 +0200

    Create Pokemon repository

---
+++ Data/Repositories/PokemonRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WhosThatPokemon.Data.Repositories
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PokemonRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task Add(Pokemon pokemon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Pokemon>> GetAll()
+        {
+            return await _db.Pokemons.ToListAsync();
+        }
+
+        public async Task<Pokemon> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Update(Pokemon pokemon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Handlers/QueryHandlers/GetAllPokemonsQueryHandler.cs
+++ Handlers/QueryHandlers/GetAllPokemonsQueryHandler.cs
@@ -1,23 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WhosThatPokemon.Data.Repositories;
 using WhosThatPokemon.Queries;
 
 namespace WhosThatPokemon.Handlers.QueryHandlers
 {
     public class GetAllPokemonsQueryHandler : IRequestHandler<GetAllPokemonsQuery, ServiceResponse<List<GetPokemonDto>>>
     {
-        private readonly ApplicationDbContext _db;
+        private readonly IPokemonRepository _pokemonRepository;
         private readonly IMapper _mapper;
 
-        public GetAllPokemonsQueryHandler(ApplicationDbContext db, IMapper mapper)
+        public GetAllPokemonsQueryHandler(IPokemonRepository pokemonRepository, IMapper mapper)
         {
-            _db = db;
+            _pokemonRepository = pokemonRepository;
             _mapper = mapper;
         }
 
         public async Task<ServiceResponse<List<GetPokemonDto>>> Handle(GetAllPokemonsQuery request, CancellationToken cancellationToken)
         {
             var serviceResponse = new ServiceResponse<List<GetPokemonDto>>();
-            var pokemons = await _db.Pokemons.ToListAsync();
+            var pokemons = await _pokemonRepository.GetAll();
             serviceResponse.Data = pokemons.Select(p => _mapper.Map<GetPokemonDto>(p)).ToList();
             serviceResponse.Message = "All pokemons retrieved successfully.";
             return serviceResponse;
