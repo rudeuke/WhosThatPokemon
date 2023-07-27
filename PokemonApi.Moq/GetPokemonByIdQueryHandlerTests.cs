b43f8946e04f30822c45042e4741f16dee6b4c07
BK <bartekqql@gmail.com>
2023-07-27 11:53:12 +0200

    Add testing project

---
+++ PokemonApi.Moq/GetPokemonByIdQueryHandlerTests.cs
@@ -0,0 +1,16 @@
+namespace PokemonApi.Moq
+{
+    public class GetPokemonByIdQueryHandlerTests
+    {
+        [Fact]
+        public async Task Handle_ShouldReturnPokemon_WhenPokemonExists() 
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
